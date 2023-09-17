ba7282494586c5d6eeb12d2584ddc120c3a4ce89
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-09-17 23:26:50 +0800

    reday

--- cubao_tippecanoe/__main__.py
+++ cubao_tippecanoe/__main__.py
@@ -1,17 +1,22 @@
 import sys
 import os
-import cubao_tippecanoe
 import glob
-
-__PWD = os.path.abspath(os.path.dirname(__file__))
+from pprint import pprint
+import subprocess
 
 
 if __name__ == '__main__':
-    print(cubao_tippecanoe.__path__, 'die')
-    print(sys.argv)
-    # - tippecanoe
-    # - decode
-    # - enumerate
-    # - jsontool
-    # - overzoom
-    # - tile_join
+    import _pybind11_tippecanoe
+    libdir = os.path.dirname(_pybind11_tippecanoe.__file__)
+    prefix = '__cubao_tippecanoe_'
+    exes = sorted(glob.glob(f'{libdir}/{prefix}*.exe'))
+    exes = {e.rsplit('/', 1)[-1][len(prefix):].split('.')[0]: e for e in exes}
+    if len(sys.argv) > 1 and sys.argv[1] not in exes:
+        exe = exes[sys.argv[1]]
+        print(f'exe: {exe}')
+        args = sys.argv[2:]
+        print(f'args: {args}')
+        subprocess.check_call([exe, *args])
+    else:
+        print('commands are:', '\n\t'.join(exes.keys()))
+        exit(-1)
